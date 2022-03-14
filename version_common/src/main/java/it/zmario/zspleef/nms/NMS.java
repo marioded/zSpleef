93068d0507c55df01ecf2db6df05190f568227ef
Mario <mariodedo08@gmail.com>
2022-03-14 15:34:15 +0100

    Final commit

--- version_common/src/main/java/it/zmario/zspleef/nms/NMS.java
+++
@@ -1,9 +0,0 @@
-package it.zmario.zspleef.nms;
-
-import org.bukkit.entity.Player;
-
-public interface NMS {
-
-    void sendTitle(Player player, String title, String subtitle, int fadeIn, int stay, int fadeOut);
-
-}
